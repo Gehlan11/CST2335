9cb8c1fcc5dc9531e37400d46748e831d5eceb41
Abdurrahman Moussa <mous0158@algonquinlive.com>
2020-07-18 16:49:53 +0530

    API call for Lyrics search

---
+++ app/src/main/java/com/example/cst2335/lyrics_ovh/ShowLyricsActivity.java
@@ -0,0 +1,17 @@
+package com.example.cst2335.lyrics_ovh;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cst2335.R;
+
+public class ShowLyricsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_lyrics);
+    }
+}
